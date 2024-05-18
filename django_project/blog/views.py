162bdc1ae73ed6713863349909fad80ff60d3af2
KennyKvn001 <k.mugisha@alustudent.com>
2024-05-18 23:29:04 +0200

    adding Templates to our app

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+posts = [
+    {
+        "author": "KEVIN",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "August 27, 2018",
+    },
+    {
+        "author": "KENNY",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "August 27, 2018",
+    },
+]
 
 
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {"posts": posts}
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, "blog/about.html", {"title": "About"})
