3697dcd88aab9d1e5ddfafb9dfd1d1ab940b1d42
KennyKvn001 <k.mugisha@alustudent.com>
2024-05-23 19:23:45 +0200

    Database & migrations

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,24 +1,9 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        "author": "KEVIN",
-        "title": "Blog Post 1",
-        "content": "First post content",
-        "date_posted": "August 27, 2018",
-    },
-    {
-        "author": "KENNY",
-        "title": "Blog Post 2",
-        "content": "Second post content",
-        "date_posted": "August 27, 2018",
-    },
-]
+from .models import Post
 
 
 def home(request):
-    context = {"posts": posts}
+    context = {"posts": Post.objects.all()}
     return render(request, "blog/home.html", context)
 
 
