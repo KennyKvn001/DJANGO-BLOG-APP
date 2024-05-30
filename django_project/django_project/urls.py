a25914abcc49619ae9e42a397ed88fffd911ab39
KennyKvn001 <k.mugisha@alustudent.com>
2024-05-30 12:52:53 +0200

    Adding login and logout functionality

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -16,11 +16,23 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("register/", user_views.register, name="register"),
+    path("profile/", user_views.profile, name="profile"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="users/login.html"),
+        name="login",
+    ),
+    path(
+        "logout/",
+        user_views.logout_view,
+        name="logout",
+    ),
     path("", include("blog.urls")),
 ]
--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 from .forms import UserRegisterForm
 
 
@@ -9,8 +11,21 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
-            messages.success(request, f"Account created for {username} succcessfully!")
-            return redirect("blog-home")
+            messages.success(
+                request, f"Your account has been created! you are now able to login"
+            )
+            return redirect("login")
     else:
         form = UserRegisterForm()
     return render(request, "users/register.html", {"form": form})
+
+
+def logout_view(request):
+    if request.method == "GET":
+        logout(request)
+        return render(request, "users/logout.html")
+
+
+@login_required
+def profile(request):
+    return render(request, "users/profile.html")
